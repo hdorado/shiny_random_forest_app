e8c33baa53d2adf933503a90979d70f4af8c72d4
hdorado <hugo.doradob@gmail.com>
2016-02-11 08:59:32 -0500

    Traducción a español y parrafos faltantes completados

--- ui.R
+++ ui.R
@@ -9,15 +9,19 @@ shinyUI(
         
             column(12,
                
-            titlePanel(strong("Exploratory analysis and variable importance 
-                                 for multivariate data:")),
+            titlePanel(strong("Análisis exploratorio e importancia de variables 
+                              en un conjunto de datos multivariado:")),
                br(),
                
-               p("This application is for exploring data sets using random forest models. 
-                 Once the user has loaded a tidy database, 
-                 immediately can start to explore the correlation between two 
-                 variables and identify the subset of relevance factors."),
-            br()     
+               p("Esta aplicación es útil para explorar bases de datos con 
+                 salidas numéricas usando análisis bi variado y la importancia 
+                 de variables en el algoritmo random forest."),
+            br(),     
+            p("A continuación, el usuario puede cargar los datos que quiere explorar; es 
+               necesario que estén contenidos dentro de un archivo csv y que 
+               mantengan un orden en el que las primeras variables sean las 
+               entradas del modelo y la última la salida:"),
+            br()  
           )
         ),
         fluidRow(
@@ -37,8 +41,15 @@ shinyUI(
         fluidRow(
             column(12,
                 div(tableOutput("baseDatos"),align="center"),
-            h3(strong('Exploratory data analysis'),align = "center" ),
+            h3(strong('Análisis exploratorio de datos'),align = "center" ),
             br(),
+            p('Se puede generar un diagrama de dispersión para variables 
+              cuantitativas y un diagrama de cajas y alambres para variables 
+              cualitativas.
+
+                El usuario puede seleccionar las variables que quiere
+               incluir en el gráfico. Cuando ‘X’ y ‘Y’ son cuantitativos, una 
+                suavización loess será agregada al gráfico.'),
             br()
             )        
         ),
@@ -54,6 +65,11 @@ shinyUI(
             column(12,
                 plotOutput("plotDescrip"),
                 h3(strong("Importance relevance\nRandom Forest"),align="center"),
+                br(),
+                p('A continuación se puede apreciar el orden de importancia en 
+                  las variables de entrada. Entre más alto sea el valor, más 
+                  relevante es en la explicación de la variable de salida.'),
+                
                 plotOutput("plotRelevance")
             )
         )
